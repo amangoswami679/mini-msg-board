20001f4a97e43700eb3932b34e6e95be679bb508
Aman Goswami <41533392+amangoswami679@users.noreply.github.com>
2024-03-22 21:04:29 +0530

    Add: New routes & messages array

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Rupert",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Sandy",
+    added: new Date()
+  }
+];
+
 const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
+});
+
+router.get('/new', function (req, res, next) {
+  res.render('form', { title: 'New Message Form' });
+});
+
+router.post('/new', function (req, res, next) {
+  const incomingReq = req.body;
+
+  const newMessage = {
+    text: incomingReq.text,
+    user: incomingReq.user,
+    added: new Date(),
+  };
+
+  messages.push(newMessage);
+
+  res.redirect('/');
 });
 
 module.exports = router;
